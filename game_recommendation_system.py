08347f0a0b99dc8671f47f50373c5938bb0ae28f
Lucas Ferreira <lfrferreira@tjma.jus.br>
2023-06-23 09:17:03 -0300

    Finalizada interação por console

--- game_recommendation_system.py
+++ game_recommendation_system.py
@@ -11,11 +11,26 @@ class GameRecommendationSystem:
     TOP_N_RECOMMENDATIONS = 5
 
     def __init__(self, data_url):
+        """
+        Inicializa a classe GameRecommendationSystem.
+
+        Args:
+            data_url (str): A URL para o arquivo de dados dos jogos.
+        """
         self.data = self.load_data(data_url)
         self.m = self.calculate_threshold()
 
     @lru_cache(maxsize=None)
     def load_data(self, url):
+        """
+        Carrega os dados dos jogos a partir de uma URL.
+
+        Args:
+            url (str): A URL para o arquivo de dados dos jogos.
+
+        Returns:
+            pandas.DataFrame: Os dados dos jogos.
+        """
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -25,28 +40,73 @@ class GameRecommendationSystem:
             raise ValueError("Erro ao carregar os dados: {}".format(e))
 
     def preprocess_data(self):
+        """
+        Pré-processa os dados dos jogos, convertendo as colunas relevantes para tipos adequados e removendo valores nulos.
+        """
         self.data['positive_ratings'] = pd.to_numeric(self.data['positive_ratings'], errors='coerce')
         self.data['negative_ratings'] = pd.to_numeric(self.data['negative_ratings'], errors='coerce')
         self.data.dropna(subset=['positive_ratings', 'negative_ratings'], inplace=True)
 
     def calculate_mean_rating(self):
+        """
+        Calcula a média das avaliações positivas dos jogos.
+
+        Returns:
+            float: A média das avaliações positivas.
+        """
         mean_rating = self.data['positive_ratings'].mean()
         return mean_rating
 
     def calculate_score(self, row, mean_rating):
+        """
+        Calcula o escore de um jogo com base nas avaliações positivas, avaliações negativas e na média das avaliações positivas.
+
+        Args:
+            row (pandas.Series): Uma linha do DataFrame contendo os dados de um jogo.
+            mean_rating (float): A média das avaliações positivas.
+
+        Returns:
+            float: O escore do jogo.
+        """
         v = row['positive_ratings']
         r = row['negative_ratings']
         return (v / (v + self.m) * r) + (self.m / (self.m + v) * mean_rating)
 
     def calculate_threshold(self):
+        """
+        Calcula o limite inferior para a quantidade de avaliações positivas dos jogos.
+
+        Returns:
+            float: O limite inferior para a quantidade de avaliações positivas.
+        """
         return self.data['positive_ratings'].quantile(self.QUANTILE)
 
     def filter_games(self):
+        """
+        Filtra os jogos com base no limite inferior para a quantidade de avaliações positivas.
+
+        Returns:
+            pandas.DataFrame: Os jogos filtrados.
+        """
         filtered_games = self.data[self.data['positive_ratings'] >= self.m].copy()
         return filtered_games
 
     @lru_cache(maxsize=None)
     def get_game_recommendations(self, title=None, genre=None, top_n=None, price=None, release_date=None, platforms=None):
+        """
+        Obtém recomendações de jogos com base nos critérios fornecidos.
+
+        Args:
+            title (str): O título desejado para os jogos.
+            genre (str): O gênero desejado para os jogos.
+            top_n (int): O número de recomendações desejado.
+            price (float): O preço máximo desejado para os jogos.
+            release_date (str): A data de lançamento mínima desejada no formato 'AAAA-MM-DD'.
+            platforms (str): As plataformas desejadas para os jogos.
+
+        Returns:
+            pandas.DataFrame: As recomendações de jogos.
+        """
         if top_n is not None and (not isinstance(top_n, int) or top_n <= 0):
             raise ValueError("O valor de 'top_n' deve ser um número inteiro positivo.")
 
@@ -71,5 +131,15 @@ class GameRecommendationSystem:
         return recommendations
 
     def get_game_details(self, game_id):
+        """
+        Obtém os detalhes de um jogo com base no ID fornecido.
+
+        Args:
+            game_id (int): O ID do jogo desejado.
+
+        Returns:
+            pandas.DataFrame: Os detalhes do jogo.
+        """
         game = self.data[self.data['appid'] == game_id]
-        return game
+        return game[['name', 'price', 'release_date', 'platforms', 'genres']]
+
--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 from game_recommendation_system import GameRecommendationSystem
+from user_input import get_user_inputs, clear_screen, get_user_inputs_id
 
 # Uso da classe GameRecommendationSystem
 data_url = 'https://drive.google.com/uc?export=download&id=1SdCK7a5E9_vQKTEmMtEROrq-LBsKlTPE'
 
 game_system = GameRecommendationSystem(data_url)
+clear_screen()
 
 try:
     game_system.preprocess_data()
 
-    # Solicitar inputs do usuário
-    genre = input("Digite o gênero desejado (ou deixe em branco para não filtrar por gênero): ")
-    top_n = int(input("Digite o número de recomendações desejado (ou deixe em branco para o valor padrão): ") or game_system.TOP_N_RECOMMENDATIONS)
+    # Obter inputs do usuário
+    title, genre, top_n, price, release_date, platforms = get_user_inputs()
+    clear_screen()
 
     # Obter recomendações com base nos inputs do usuário
-    recommendations = game_system.get_game_recommendations(genre=genre, top_n=top_n)
+    recommendations = game_system.get_game_recommendations(title=title, genre=genre, top_n=top_n, price=price, release_date=release_date, platforms=platforms)
     print(recommendations)
 
-    print("=====================================================================================")
-    game_id = int(input("Digite o ID (appid) do jogo para obter detalhes: "))
+    game_id = get_user_inputs_id()
 
     # Obter detalhes do jogo com base no ID fornecido pelo usuário
     game_details = game_system.get_game_details(game_id)
     print(game_details)
 except ValueError as e:
     print("Ocorreu um erro:", str(e))
-except Exception as e:
-    print("Ocorreu um erro não esperado:", str(e))
-
